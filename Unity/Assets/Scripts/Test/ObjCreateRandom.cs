f98ba6c4b78f66c1d93b0713261be1ac59b2f3bf
keisuke_watanabe <keisuke_watanabe@dwango.co.jp>
2015-01-17 14:26:51 +0900

    アイテムのスクリプトと当たったアイテムの判定を追加

--- Unity/Assets/Scripts/Test/ObjCreateRandom.cs
+++ Unity/Assets/Scripts/Test/ObjCreateRandom.cs
@@ -1,19 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class ObjCreateRandom : MonoBehaviour {
-	private GameObject go;
+	private GameObject[] goItems;
 	private GameObject goCreated;
 	private GameObject goParent;
 
 	private float intervalTime;
 
-	private const string GO_OBSTACLE = "Prefabs/Test/GOD_Obstacle";
+	private readonly string[] GO_ITEM_PATHS = new[] {
+		"Prefabs/MainGame/Item/GOD_ItemDamage",
+		"Prefabs/MainGame/Item/GOD_ItemLifeUp",
+		"Prefabs/MainGame/Item/GOD_ItemSpeedDown",
+		"Prefabs/MainGame/Item/GOD_ItemSpeedUp"
+	};
 	private const string GO_PARENT = "UI Root/Camera/Panel";
 
 	// Use this for initialization
 	void Start () {
-		go = (GameObject)Resources.Load (GO_OBSTACLE);
+		goItems = GO_ITEM_PATHS.Select(p => Resources.Load(p)).Cast<GameObject>().ToArray();
 		goParent = GameObject.Find (GO_PARENT);
 	}
 	
@@ -22,7 +28,7 @@ public class ObjCreateRandom : MonoBehaviour {
 		intervalTime += Time.deltaTime;
 
 		if (intervalTime >= 2.0f) {
-			goCreated = (GameObject) Instantiate (go);
+			goCreated = (GameObject)Instantiate(goItems[Random.Range(0, goItems.Length)]);
 			goCreated.transform.parent = goParent.transform;
 
 			intervalTime = 0.0f;
--- Unity/Assets/Scripts/Test/Player.cs
+++ Unity/Assets/Scripts/Test/Player.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-
 	private Vector3 speed = new Vector3(0, 2f, 0);
 
 	// Use this for initialization
@@ -25,6 +24,23 @@ public class Player : MonoBehaviour {
 
 	//　物体の衝突時の処理
 	void OnTriggerEnter2D(Collider2D go) {
-		Debug.Log (this.gameObject.name + "←" + go.gameObject.name);
+		Item item = (Item)go.gameObject.GetComponent(typeof(Item));
+		string message = "";
+		switch (item.type)
+		{
+		case Item.ItemType.Damage:
+			message = "Damage";
+			break;
+		case Item.ItemType.LifeUp:
+			message = "LifeUp";
+			break;
+		case Item.ItemType.SpeedDown:
+			message = "SpeedDown";
+			break;
+		case Item.ItemType.SpeedUp:
+			message = "SpeedUp";
+            break;
+        }
+        Debug.Log(message + " " + this.gameObject.name + "←" + go.gameObject.name);
 	}
 }
