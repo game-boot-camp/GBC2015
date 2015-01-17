b8a0c14aaf1b5bf54fa3f7e1317ea5c2787f0300
permil <mame9696@hotmail.com>
2015-01-18 02:26:04 +0900

    スクロールスピードを修正

--- Unity/Assets/Scripts/MainGame/Move/ReflectMover.cs
+++ Unity/Assets/Scripts/MainGame/Move/ReflectMover.cs
@@ -14,7 +14,7 @@ public class ReflectMover : MonoBehaviour {
 		PauseState pauseState = GameObject.Find("Pause").GetComponent<PauseState>();
 		if (pauseState.paused) { return; }
 
-		gameObject.transform.localPosition += new Vector3(-1f * Time.deltaTime, speed * Time.deltaTime, 0.0f);
+		gameObject.transform.localPosition += new Vector3(-100f * Time.deltaTime, speed * Time.deltaTime, 0.0f);
 		if ((this.transform.localPosition.y >= 200 && speed > 0f) || (this.transform.localPosition.y <= -200 && speed < 0f)) {
 			speed = -speed;
 		}
