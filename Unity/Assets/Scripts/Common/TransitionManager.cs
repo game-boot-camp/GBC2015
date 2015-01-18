15bd7626ec9fa2e2039add20ebb327bc1f63c906
mutsuu123 <mutsuu.stacc@gmail.com>
2015-01-18 13:49:30 +0900

    遊び方の処理を追加、タイトルの差し替え、選択メニュー画面の差し替え

--- Unity/Assets/Scripts/Common/TransitionManager.cs
+++ Unity/Assets/Scripts/Common/TransitionManager.cs
@@ -4,23 +4,31 @@ using System.Collections;
 public class TransitionManager : MonoBehaviour {
 
 	private GameObject goPanel;
+	private GameObject goFadeTamplate;
 	private GameObject goFade;
 
 	private const string PANEL_PATH = "UI Root/Camera/Panel";
 	private const string FADE_PATH = "Prefabs/Common/GOD_Loading";
+	private const string FADE_ANIM_NAME = "Anim_Loading";
 
 	void Awake() {
-		goPanel = GameObject.Find (PANEL_PATH);
-		goFade = (GameObject)Resources.Load (FADE_PATH);
+
 	}
 
 	// Use this for initialization
 	void Start () {
-	
+		goPanel = GameObject.Find (PANEL_PATH);
+		goFadeTamplate = (GameObject)Resources.Load (FADE_PATH);
+		goFade = (GameObject)Instantiate(goFadeTamplate);
+		
+		goFade.transform.parent = goPanel.transform;
+		goFade.transform.localScale = new Vector3 (1.0f,1.0f, 1.0f);
+
+		goFade.animation.Play (FADE_ANIM_NAME);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
