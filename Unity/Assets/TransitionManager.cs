8f82af969babf0e1375cac29e7b7a14e948ce751
mutsuu123 <mutsuu.stacc@gmail.com>
2015-01-18 11:48:17 +0900

    キャラ選択のSceneを修正

---
+++ Unity/Assets/TransitionManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransitionManager : MonoBehaviour {
+
+	private GameObject goPanel;
+	private GameObject goFade;
+
+	private const string PANEL_PATH = "UI Root/Camera/Panel";
+	private const string FADE_PATH = "Prefabs/Common/GOD_Loading";
+
+	void Awake() {
+		goPanel = GameObject.Find (PANEL_PATH);
+		goFade = (GameObject)Resources.Load (FADE_PATH);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
