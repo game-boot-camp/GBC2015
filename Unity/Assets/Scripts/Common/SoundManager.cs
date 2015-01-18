cc7152d2891e02c2c42b4a7050146ee320c5d6e1
mutsuu123 <mutsuu.stacc@gmail.com>
2015-01-18 09:58:26 +0900

    サウンドを追加

---
+++ Unity/Assets/Scripts/Common/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+	private AudioSource audioSource;
+	public AudioClip audioClip;
+
+	void Awake() {
+		DontDestroyOnLoad(this);
+
+		audioSource = gameObject.GetComponent<AudioSource> (); 
+		audioSource.clip = audioClip;
+		audioSource.Play ();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
